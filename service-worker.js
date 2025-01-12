8dedc9c9facda3c6870446e37e5a8573e9f2b651
pezzaliapp <pezzalialessandro@gmail.com>
2025-01-12 20:46:52 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,41 @@
-// Durante l'installazione, il Service Worker mette in cache i file necessari
-self.addEventListener('install', (event) => {
-  console.log('[Service Worker] Installing...');
-  event.waitUntil(
-    caches.open('biorhythm-cache').then((cache) => {
-      console.log('[Service Worker] Caching app shell and content');
-      return cache.addAll([
-        './',
-        './index.html',
-        './app.js',
-        // Se hai altri file (CSS, immagini, ecc.), aggiungili qui
-      ]);
-    })
-  );
+const CACHE_NAME = "biorhythm-cache-v1";
+const ASSETS = [
+    "./index.html",
+    "./styles.css",
+    "./app.js",
+    "./manifest.json",
+    "./images/icon-192x192.png",
+    "./images/icon-512x512.png"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("Caching assets");
+            return cache.addAll(ASSETS);
+        })
+    );
 });
 
-// Attivazione del Service Worker
-self.addEventListener('activate', (event) => {
-  console.log('[Service Worker] Activating...');
-  // Se devi gestire la cancellazione di vecchie cache, fallo qui
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        console.log("Clearing old cache");
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
 });
 
-// Intercetta le richieste e serve i file dalla cache se disponibili
-self.addEventListener('fetch', (event) => {
-  console.log('[Service Worker] Fetching...', event.request.url);
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      // Ritorna la risposta dalla cache se presente, altrimenti effettua un fetch
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
